4181e2cd9bab2925bbc64d5c9cdfbe92cc433e67
Aarushi <aarushimandloi744@gmail.com>
2025-11-18 22:18:49 +0530

    lecture-1_TypeScript

---
+++ typesrcipt/basics.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 10;
+let c = "hello";
+//# sourceMappingURL=basics.js.map
---
+++ typesrcipt/basics.ts
@@ -0,0 +1,3 @@
+let a:number=10;
+let c:string="hello";
+
