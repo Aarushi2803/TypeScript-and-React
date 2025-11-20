06acf2ceeefe7982bfa7d8cd9d17f14bdb09ef65
Aarushi <aarushimandloi744@gmail.com>
2025-11-20 21:50:15 +0530

    basics-2

--- typesrcipt/basics.js
+++ typesrcipt/basics.js
@@ -2,4 +2,38 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = 10;
 let c = "hello";
+let b;
+b = 20;
+b = "string";
+console.log(b.toUpperCase());
+let d;
+d = 30;
+d = "sring";
+// console.log(d.toUpperCase()); // Error: Object is of type 'unknown'.
+if (typeof d === "string") {
+    console.log(d.toUpperCase());
+} // then it works
+//Non-primitive types
+let arr = [1, 2, 3, 4];
+let arr1 = [1, 2, 3, 4]; // type inference
+let arr2 = [1, "hello"];
+let arr3 = [1, "hello", true];
+let obj = {
+    name: "John",
+    age: 30
+};
+let obj1;
+obj1 = { name: "Doe", age: 25 };
+let c1 = {
+    name: "Alice",
+    age: 28,
+    id: 101
+};
+// interface merging but type is not merging 
+let obj3 = {
+    name: "Bob",
+    age: 35,
+    id: 202,
+    department: "HR"
+};
 //# sourceMappingURL=basics.js.map
--- typesrcipt/basics.ts
+++ typesrcipt/basics.ts
@@ -1,3 +1,61 @@
 let a:number=10;
 let c:string="hello";
 
+let b:any;
+b=20;
+b="string";
+console.log(b.toUpperCase());
+
+let d:unknown;
+d=30;
+d="sring";
+// console.log(d.toUpperCase()); // Error: Object is of type 'unknown'.
+
+if(typeof d === "string"){
+    console.log(d.toUpperCase());
+} // then it works
+
+//Non-primitive types
+
+let arr:number[]=[1,2,3,4];
+let arr1=[1,2,3,4]; // type inference
+
+let arr2:(number|string)[]=[1,"hello"];
+let arr3:(number|string|boolean)[]=[1,"hello",true];
+
+let obj:{name:string, age:number}={
+        name:"John",
+        age:30
+};
+
+let obj1:{name:string;age:number}; 
+obj1={name:"Doe", age:25};
+
+type customer = {
+    name:string, 
+    age:number,
+    id:number
+} 
+
+let c1:customer={
+    name:"Alice",
+    age:28,
+    id:101
+};
+
+interface Employee{
+    name:string,
+    age:number,
+    id:number
+}
+interface Employee{
+    department:string 
+}
+
+// interface merging but type is not merging 
+let obj3:Employee={
+    name:"Bob",
+    age:35,
+    id:202,
+    department:"HR"
+};
