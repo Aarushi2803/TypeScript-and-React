c32846f099cfe7547ec93a6c084f873cf9d72e88
Aarushi <aarushimandloi744@gmail.com>
2025-11-24 23:17:10 +0530

    classes

--- typesrcipt/interface.ts
+++ typesrcipt/interface.ts
@@ -40,3 +40,32 @@ const obj3:Required<customer>={
     something:"value"
 }
 
+class Product {
+    pid: number;
+    name: string;
+    price: number;
+
+    constructor(pid: number, name: string, price: number) {
+        this.pid = pid;
+        this.name = name;
+        this.price = price;
+    }
+
+    display(): void {
+        console.log(`Product ID: ${this.pid}, Name: ${this.name}, Price: ₹${this.price}`);
+    }
+}  
+
+class DigitalProduct extends Product {
+    fileSizeMB: number;
+
+    constructor(pid: number, name: string, price: number, fileSizeMB: number) {
+        super(pid, name, price); // calls Product constructor
+        this.fileSizeMB = fileSizeMB;
+    }
+
+    display(): void {
+        super.display(); // reuse base method
+        console.log(`File Size: ${this.fileSizeMB} MB`);
+    }
+} 
